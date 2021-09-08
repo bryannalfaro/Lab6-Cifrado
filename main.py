7ddc408de6e46da5e76fbf597eb173332131e2a0
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-09-08 08:14:29 -0600

    feat: 3ra parte hecha

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import hashlib
 import base64
 from producer import *
 
-
+'''
 # sha256
 text =  'Hola'.encode('utf-8')
 print(hashlib.sha256(text).digest())
@@ -41,6 +41,8 @@ print(hashlib.blake2b(text,digest_size=digest_size).digest())
 print(hashlib.blake2b(text,digest_size=digest_size).hexdigest())
 print(base64.b64encode(hashlib.blake2b(text,digest_size=digest_size).digest()).decode())
 
+
+#2da parte
 hash1 = produce_hash('texto1.txt','h'.encode('utf-8'))
 hash2 = produce_hash('texto2.txt','h'.encode('utf-8'))
 
@@ -50,4 +52,23 @@ print('Hash2: ',hash2)
 print(verify('texto1.txt','h'.encode('utf-8'),hash1))
 print(verify('texto1.txt','h'.encode('utf-8'),hash2))
 print(verify('texto2.txt','h'.encode('utf-8'),hash1))
-print(verify('texto2.txt','h'.encode('utf-8'),hash2))
+print(verify('texto2.txt','h'.encode('utf-8'),hash2))'''
+
+
+#3era parte
+print('SERVIDOR CIFRADO DE LA INFORMACION')
+print('1. Registrarse')
+print('2. Login')
+
+opcion = int(input('Ingresa el numero de opcion: '))
+if opcion ==1:
+
+    user  = input('Ingresa usuario: ')
+    password = input('Ingresa password: ')
+
+    register(user,password)
+    print('Usuario registrado')
+elif opcion ==2:
+    user  = input('Ingresa usuario: ')
+    password = input('Ingresa password: ')
+    print(login(user,password))
--- producer.py
+++ producer.py
@@ -1,5 +1,7 @@
 import hashlib
 import hmac
+from Crypto import Random
+import csv
 
 
 def produce_hash(filename,key):
@@ -21,6 +23,38 @@ def verify(filename,key,hashf):
     else:
         file.close()
         return 'No es igual'
+
+#3ra parte - Login y registro
+
+def register(username,password):
+    salt = Random.get_random_bytes(32)
+
+    text = (password+salt.hex()).encode('utf-8')
+    hashf = hashlib.sha256(text).hexdigest()
+
+    with open('db.csv', mode='a',newline='') as user_f:
+        user_writer = csv.writer(user_f)
+
+        user_writer.writerow([username,salt.hex(),hashf])
+
+def login(username,password):
+    csv_file = csv.reader(open('db.csv', "r"))
+    find = False
+    for row in csv_file:
+
+        if username == row[0]:
+            find = True
+            text = (password+row[1]).encode('utf-8')
+            hashf = hashlib.sha256(text).hexdigest()
+            if(hashf==row[2]):
+                return 'Bienvenido al servidor ' + username
+            else:
+                return 'Contraseña incorrecta'
+    if find == False:
+        return 'Usuario no encontrado'
+
+
+
 
 
 
