63387d06f4d80b194f0feb0908c38b72c1c500b2
Bryann Alfaro <alf19372@uvg.edu.gt>
2021-09-07 20:01:20 -0600

    feat: first part done

--- main.py
+++ main.py
@@ -13,3 +13,29 @@ text =  'Hola'.encode('utf-8')
 print(hashlib.sha512(text).digest())
 print(hashlib.sha512(text).hexdigest())
 print(base64.b64encode(hashlib.sha256(text).digest()).decode())
+
+#blake2b
+text =  'Hola'.encode('utf-8')
+digest_size = 32
+print(hashlib.blake2b(text,digest_size=digest_size).digest())
+print(hashlib.blake2b(text,digest_size=digest_size).hexdigest())
+print(base64.b64encode(hashlib.blake2b(text,digest_size=digest_size).digest()).decode())
+
+# sha256
+text =  'Hola este es texto para cifrado 2'.encode('utf-8')
+print(hashlib.sha256(text).digest())
+print(hashlib.sha256(text).hexdigest())
+print(base64.b64encode(hashlib.sha256(text).digest()).decode())
+
+#sha512
+text =  'Hola este es texto para cifrado 2'.encode('utf-8')
+print(hashlib.sha512(text).digest())
+print(hashlib.sha512(text).hexdigest())
+print(base64.b64encode(hashlib.sha256(text).digest()).decode())
+
+#blake2b
+text =  'Hola este es texto para cifrado 2'.encode('utf-8')
+digest_size = 32
+print(hashlib.blake2b(text,digest_size=digest_size).digest())
+print(hashlib.blake2b(text,digest_size=digest_size).hexdigest())
+print(base64.b64encode(hashlib.blake2b(text,digest_size=digest_size).digest()).decode())
